0c2a2d3a637d65b83afe32984509ec1fff90c303
tantangin <irsan00mansyur@gmail.com>
2020-02-05 13:47:17 -0400

    notif toast

--- application/controllers/Aspirasi.php
+++ application/controllers/Aspirasi.php
@@ -54,6 +54,9 @@ class Aspirasi extends MY_Controller
         $this->db->insert($tbl['name'], $data);
         $eks = hasilCUD("Sukses");
         $type = $eks->status ? 'success' : 'danger';
+
+        $this->session->set_flashdata('message', '<div style="position: absolute; top:50px; right: 5px;z-index:9999999"><div class="toast fade show"><div class="toast-header bg-' . $type . '"><strong class="mr-auto"><i class="fa fa-globe"></i> Berhasil.!</strong><small class="text-muted">just now</small><button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button></div><div class="toast-body">' . $eks->message . '.!</div></div></div>');
+
         $this->session->set_flashdata('message', "<div class='mb-5 alert alert-$type' role='alert'>$eks->message .!</div>");
         redirect(base_url("aspirasi/id/" . $this->input->post('aspirasi_id')));
     }
