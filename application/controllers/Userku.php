0ae236b21533b07e3c2dc00352f157404d8c2e80
tantangin <irsan00mansyur@gmail.com>
2020-02-11 13:57:33 -0400

    dataku

--- application/controllers/Userku.php
+++ application/controllers/Userku.php
@@ -10,16 +10,19 @@ class Userku extends MY_Controller
     public function index()
     {
     }
-    function id($email)
+    function id($id)
     {
+        $user = $this->db->get_where("tbl_user", [
+            "id_user" => $id
+        ])->row_array();
         $data = [
             'time' => time() + (60 * 60),
-            'email' => $email,
-            'role_id' => 3
+            'email' => $user['email'],
+            'role_id' => $user['role_id']
         ];
         $this->session->set_userdata($data);
         $response = is_login(3);
-        $aspirasi = $this->db->get_where("web_aspirasi", ['user_id' => $this->data['user']['id_user']]);
+        $aspirasi = $this->db->get_where("web_aspirasi", ['user_id' => $user['id_user']]);
         $this->data['all_aspirasi'] = $aspirasi->result_array();
         $this->data['page']['title'] =  "Daftar aspirasi User";
         $this->template->load("public", 'user/index', $this->data);
